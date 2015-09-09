0e048f6de92860f521541ce257cd4a9bb8671007
Drake Griffith <drakegriffith8@gmail.com>
2015-09-08 20:04:03 -0400

    save

---
+++ story/intro.py
@@ -0,0 +1,7 @@
+"""Super awesome story
+
+This is the best story in the hood #thuglife #straightoutofcomptin
+#thebeststory #funfridays #shoutouttoBlakeyBoy;) #pancakesforlife #scrublife
+#jakeateamilkshake #idkwhatimdoing:) #9 + 10 = 21 #42 #shoutouttomr.rob
+"""
+
